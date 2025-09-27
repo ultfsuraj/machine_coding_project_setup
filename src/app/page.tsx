91676dc7e002eb8994edd7f36088c6e1c62ea4b8
R B suraj <rbsuraj432@gmail.com>
2025-09-27 14:02:24 +0530

    setup rtk, twmerge, clsx, cva, motion, tanstack react query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { cn } from '@/utils/util';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div
+      className={cn(
+        'flex h-20 w-20 items-center justify-center',
+        'm-auto mt-4 bg-amber-50',
+        'drop-shadow-md drop-shadow-neutral-700'
+      )}
+    >
+      Home
+    </div>
+  );
 }
